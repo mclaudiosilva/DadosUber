9a9de086da91792d88994c1ef1c0c94822ce3163
mclaudiosilva <72809242+mclaudiosilva@users.noreply.github.com>
2026-04-30 17:34:09 -0300

    chore: scaffold initial .NET Web API base structure

---
+++ DadosAplicativo.Api/Program.cs
@@ -0,0 +1,38 @@
+using DadosAplicativo.Api.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddScoped<DbConnectionFactory>();
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("OpenCorsPolicy", policy =>
+    {
+        policy
+            .AllowAnyOrigin()
+            .AllowAnyHeader()
+            .AllowAnyMethod();
+    });
+});
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseCors("OpenCorsPolicy");
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
