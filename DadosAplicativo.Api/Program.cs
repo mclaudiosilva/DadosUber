cac5aa511be07e7afd1b6730cc74f12a40c878dd
Marcos <marcos.claudio@profitsconsulting.com.br>
2026-04-30 19:30:54 -0300

    Ajustes iniciais da API

--- DadosAplicativo.Api/Program.cs
+++ DadosAplicativo.Api/Program.cs
@@ -29,11 +29,9 @@ builder.Services.AddCors(options =>
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
-{
     app.UseSwagger();
     app.UseSwaggerUI();
-}
+
 
 app.UseHttpsRedirection();
 
