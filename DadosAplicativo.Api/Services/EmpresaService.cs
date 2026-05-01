87f6271dce0aee759aed180996f05b636180939c
Marcos <marcos.claudio@profitsconsulting.com.br>
2026-04-30 22:07:03 -0300

    Removendo appsettings do git e ajustes no projeto

--- DadosAplicativo.Api/Services/EmpresaService.cs
+++ DadosAplicativo.Api/Services/EmpresaService.cs
@@ -52,8 +52,10 @@ public class EmpresaService : IEmpresaService
     {
         if (string.IsNullOrWhiteSpace(nome))
             throw new ArgumentException("Nome da empresa é obrigatório.");
-    public Task<IEnumerable<EmpresaResponse>> ListarEmpresasAsync()
-    {
-        return _empresaRepository.ListarEmpresasAsync();
     }
+
+    //public Task<IEnumerable<EmpresaResponse>> ListarEmpresasAsync()
+    //{
+    //    return _empresaRepository.ListarEmpresasAsync();
+    //}
 }
